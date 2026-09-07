a509cbc2bc451c784c5a95d710fc5c3e45fb2521
SerbCSharp <SerbCSharp@mail.ru>
2026-09-07 16:40:11 +0300

    Change InterestCost

--- Reports/Domain/InterestCost.cs
+++ Reports/Domain/InterestCost.cs
@@ -2,5 +2,14 @@
 {
     public class InterestCost
     {
+        public int Year { get; set; }
+        public int Quarter { get; set; }
+        public string Name { get; set; }
+        public string Field { get; set; }
+        public decimal Fact { get; set; }
+        public int LineNumber { get; set; }
+        public decimal TotalSales { get; set; }
+        public decimal TotalCost { get; set; }
+        public decimal Amount { get; set; }
     }
 }
