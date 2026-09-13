d701772f5f8ecbed0ed5e0ae5386db3f40ac8786
Serb <SerbCSharp@mail.ru>
2026-09-13 21:42:45 +0300

    Change Interfaces

--- ForecastingModelParameters/Application/Interfaces/IGetDataRepository.cs
+++ ForecastingModelParameters/Application/Interfaces/IGetDataRepository.cs
@@ -6,5 +6,11 @@ namespace ForecastingModelParameters.Application.Interfaces
     {
         Task<List<ProjectCostingData>> ProjectCostingDataAsync(string complexProperty);
         Task<List<ReportField>> ReportFieldAsync();
+
+        Task<IEnumerable<Revenue>> RevenueAsync(string complexProperty);
+        Task<IEnumerable<ConstructionExpense>> ConstructionExpenseAsync(string complexProperty);
+        Task<IEnumerable<OtherExpense>> OtherExpenseAsync(string complexProperty);
+        Task<IEnumerable<ReportStructure>> ReportStructureAsync();
+
     }
 }
